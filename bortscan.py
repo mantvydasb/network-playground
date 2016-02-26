fff0aed5c3e3a81debe20767cd4918a1ff072f25
mantvydas <mantvydo@gmail.com>
2016-02-26 23:42:50 +0000

    houseekping

--- bortscan.py
+++ bortscan.py
@@ -2,23 +2,22 @@ import socket
 import threading
 import sys
 
-HOST = "192.168.2.2"
-OPEN_PORTS = []
-
 def main():
-    if sys.argv[1]:
-        HOST = sys.argv[1]
+    if len(sys.argv) > 1: host = sys.argv[1]
+    else: host = "192.168.2.2"
 
-    print("Scanning %s for open ports in the range 0-65536" % HOST)
+    print("Scanning %s for open ports in the range 0-65536" % host)
     for port in range(0, 65536):
-        scanningThread = threading.Thread(target=scanPort, args=[HOST,port])
-        scanningThread.start()
+        scanningThread = threading.Thread(target=scanPort, args=[host,port])
+        scanningThread.run()
 
 def scanPort(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         result = s.connect_ex((host, port))
-        if result == 0: print("[!] Open: %s" % str(port))
+        if result == 0:
+            print("[!] Open: %s" % str(port))
+        s.close()
     except: pass
 
 main()
