f9c7d722bd17bb51dea9b265bce57b382f67cdc0
mantvydas <mantvydo@gmail.com>
2016-02-29 22:10:48 +0000

    started playing with scapy

--- scabby.py
+++ scabby.py
@@ -2,6 +2,19 @@ from scapy.all import *
 
 a = IP()
 a.src = "192.168.2.2"
-a.dst = "192.168.2.3"
-hexdump(a)
+a.dst = "192.168.2.4"
 print(a)
+
+b = TCP()
+print(b)
+
+c = ARP()
+c.op = 1
+c.psrc = "192.168.2.50"
+c.pdst = "192.168.2.60"
+c.hwdst = "30:5a:3a:57:c0:50"
+
+while True:
+    print('Sending ARP packet:' + str(c))
+    send(c)
+    time.sleep(2)
