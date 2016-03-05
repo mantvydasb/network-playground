52859820ed2367e3c41baed0c756089277587ee3
mantvydas <mantvydo@gmail.com>
2016-03-05 20:47:09 +0000

    will try to build a tcp packet

---
+++ raw_tcp_packet.py
@@ -0,0 +1,6 @@
+import socket
+
+HOST_IP = "192.168.2.2"
+TARGET_IP = "192.168.2.1"
+HOST_MAC = "30:5a:3a:57:c0:2e"
+TARGET_MAC = "00:1c:df:c1:bd:91"
--- structures.py
+++
@@ -1,9 +0,0 @@
-from ctypes import *
-
-class Int():
-    _fields_ = [("first_16", c_int, 16), ("second_16", c_int, 16)]
-
-a = Int()
-a.first_16 = 20
-a.second_16 = 40
-print(a.first_16)
