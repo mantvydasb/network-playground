4d75355822dc9282ed81f6eaad2b55bf9b55cc4e
mantvydas <mantvydo@gmail.com>
2016-03-12 15:46:16 +0000

    started forming the post request

--- bruter.py
+++ bruter.py
@@ -1,15 +1,18 @@
-import queue
-import threading
 import urllib3
+import urllib.parse
 
 class Bruter:
 
     baseUrl = "http://192.168.2.1/"
-    postUrl = baseUrl + "/cgi-bin/login.exe"
+    postUrl = baseUrl + "cgi-bin/login.exe"
+
+    poolManager = urllib3.PoolManager()
 
     def __init__(self):
         print("Starting..")
-
-
+        fields = {"pws" : self.pws}
+        req = self.poolManager.urlopen("POST", self.postUrl, headers={'content-type': 'application/x-www-form-urlencoded'}, body={'pws': self.pws})
+        response = self.poolManager.request('POST', self.postUrl, fields=fields, headers={'content-type': 'application/x-www-form-urlencoded'})
+        print(req)
 
 Bruter()
