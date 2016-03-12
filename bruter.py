6d093542b1d96ec7d87a7bb2332b9db2e8a25bc0
mantvydas <mantvydo@gmail.com>
2016-03-12 14:30:39 +0000

    started router brouter

--- bruter.py
+++ bruter.py
@@ -1,3 +1,15 @@
 import queue
 import threading
+import urllib3
 
+class Bruter:
+
+    baseUrl = "http://192.168.2.1/"
+    postUrl = baseUrl + "/cgi-bin/login.exe"
+
+    def __init__(self):
+        print("Starting..")
+
+
+
+Bruter()
