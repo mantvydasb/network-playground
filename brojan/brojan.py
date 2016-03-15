c6c5844489f0d3456b7adaa844a57b80434bb1c1
mantvydas <mantvydo@gmail.com>
2016-03-15 23:03:32 +0000

    - started implementing methods for retrieving file contents

--- brojan/brojan.py
+++ brojan/brojan.py
@@ -12,19 +12,35 @@ from github import *
 class GitHubSession():
     username = "mantvydo@gmail.com"
     username = "mantvydasb"
+    session = ''
 
     def __init__(self, username=username):
         password = input("Github password: ")
         self.session = Github(username, password)
+        self.repository = self.session.get_repo(self.username + "/network-playground")
+        self.branch = self.repository.get_branch("master")
+
+
+    def getFileContents(self, pathToFile):
+        pienas = self.repository.get_contents(pathToFile)
+        print(pienas)
+
+
+    def getConfig(self, id):
+        self.getFileContents()
 
 
 class Brojan():
+    id = "1"
+    configPath = "configs/" + id + ".json"
+    intelligenceStoragePath = "data/%s/" % id
+    modules = []
+    configured = False
+    tasksQueue = queue.Queue()
 
     def __init__(self):
         github = GitHubSession()
-        repository = github.session.get_repo(github.username + "/network-playground")
-
-
+        github.getFileContents(self.configPath)
 
 
 Brojan()
